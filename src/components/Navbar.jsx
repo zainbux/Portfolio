89856d7cf2fc9f95118f51f911b1175d535ab029
zainbux <zainbux99@gmail.com>
2023-07-17 17:04:57 -0400

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,14 +122,14 @@ const Navbar = () => {
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+          {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
               href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:e87f7bc7-5041-369d-95aa-c74484907cf8'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
